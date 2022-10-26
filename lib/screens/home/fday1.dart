8b6813fdeb7132198fa09d50815b207542b63260
Tarooba Faheem <tarooba.faheem.hussain@gmail.com>
2022-10-27 03:41:47 +0500

    Updated App.
    till youtube video player added.
    need to add congo! card after the video.

---
+++ lib/screens/home/fday1.dart
@@ -0,0 +1,369 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+import 'day1_exercises.dart';
+
+class Fday1 extends StatefulWidget {
+  @override
+  State<Fday1> createState() => _WorkoutState();
+}
+
+class _WorkoutState extends State<Fday1> {
+  late VideoPlayerController _squats;
+  late VideoPlayerController _yoga;
+  late VideoPlayerController _controller;
+  late VideoPlayerController _jumpingjacks;
+  late VideoPlayerController _crunches;
+
+  // ignore: unused_field
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _squats = VideoPlayerController.asset("assets/my_video_squats.mp4");
+    _yoga = VideoPlayerController.asset("assets/my_video_yoga.mp4");
+    _controller = VideoPlayerController.asset("assets/my_video_pushups.mp4");
+    _jumpingjacks =
+        VideoPlayerController.asset("assets/my_video_jumpingjacks.mp4");
+    _crunches = VideoPlayerController.asset("assets/my_video_crunches.mp4");
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _squats.initialize();
+    _initializeVideoPlayerFuture = _yoga.initialize();
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _initializeVideoPlayerFuture = _jumpingjacks.initialize();
+    _initializeVideoPlayerFuture = _crunches.initialize();
+
+    // Use the controller to loop the video.
+    _squats.setLooping(false);
+    _yoga.setLooping(false);
+    _controller.setLooping(false);
+    _jumpingjacks.setLooping(false);
+    _crunches.setLooping(false);
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _squats.dispose();
+    _yoga.dispose();
+    _controller.dispose();
+    _crunches.dispose();
+
+    super.dispose();
+  }
+
+  // ignore: non_constant_identifier_names
+  DialogBox(VideoPlayerController _controller) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Container(
+          width: 200,
+          height: 200,
+          child: VideoPlayer(_controller),
+        ),
+        actions: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.play_arrow),
+                onPressed: () {
+                  // Wrap the play or pause in a call to `setState`. This ensures the
+                  // correct icon is shown.
+                  setState(() {
+                    _controller.play();
+                  });
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.pause),
+                onPressed: () {
+                  // Wrap the play or pause in a call to `setState`. This ensures the
+                  // correct icon is shown.
+                  setState(() {
+                    _controller.pause();
+                  });
+                },
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _controller.pause();
+                  });
+                  Navigator.of(context).pop();
+                },
+                icon: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Close"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Day 1',
+            style: TextStyle(
+                fontSize: 21, color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+          elevation: 6.0,
+          backgroundColor: Color.fromARGB(255, 177, 96, 191),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            color: Color.fromARGB(255, 255, 255, 255),
+            icon: Icon(Icons.arrow_back),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(
+                    '6 Mins, 7 Exercises',
+                    style: TextStyle(
+                        height: 2.7,
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 135, 135, 135)),
+                  ),
+                  SizedBox(
+                    height: 1,
+                    width: 100.0,
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      DialogBox(_controller);
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      elevation: 5.0,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('SQUATS',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text('40 s',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      DialogBox(_yoga);
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      elevation: 5.0,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('Wall PushUps',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text('30 s',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      DialogBox(_controller);
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      elevation: 5.0,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('Clapping Crunches',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text('20 s',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      DialogBox(_jumpingjacks);
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      elevation: 5.0,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('JUMPING JACKS',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text('50 s',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      DialogBox(_crunches);
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      elevation: 5.0,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('Side Lying Legs Lift',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text('40 s',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    splashColor: Colors.purple[200],
+                    onTap: () {
+                      DialogBox(_crunches);
+                    },
+                    child: Card(
+                      color: Color.fromARGB(255, 249, 238, 253),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 15.0,
+                      ),
+                      elevation: 5.0,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.album,
+                          size: 65,
+                          color: Colors.purple[500],
+                        ),
+                        title: Text('Fire Hydrant',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.purple[300],
+                            )),
+                        subtitle: Text('40 s',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey[700],
+                            )),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 234, 222, 222),
+                      onPrimary: Colors.purple[300],
+                      side: BorderSide(
+                          color: Color.fromARGB(255, 133, 17, 154), width: 2),
+                    ),
+                    child: const Text(
+                      'Start',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Day1_Exercises()));
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
